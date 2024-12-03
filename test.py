64d43625a5e80beeee94ba89e7ef09f6b5003a54
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-02 21:27:49 -0500

    Test: Created a test function to find a mongoDocument in the database. Note: The find_document_by_uuid() is already created due to completing the function before the second function save_document()

--- test.py
+++ test.py
@@ -7,3 +7,11 @@ def test_save_document():
     document = {"UUID": str(uuid.uuid4()), "name": "TestDoc", "value": 200}
     save_document(document)
     assert find_document_by_uuid(document["UUID"]) == document
+
+
+
+def test_find_document_by_uuid():
+    uuid = create_random_document()
+    document = find_document_by_uuid(uuid)
+    assert document is not None
+    assert document["UUID"] == uuid   
