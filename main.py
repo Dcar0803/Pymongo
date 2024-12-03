3872bb8c814048155720d8d380e82f46faf03a58
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-02 21:07:39 -0500

    Function: Created a function that will create a random document and returns the uuid of the document.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import uuid
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client.test_database  
+collection = db.test_collection 
+
+def create_random_document():
+
+    document = {
+        "UUID": str(uuid.uuid4()),
+        "name": "RandomName",
+        "value": 100  # Arbitrary fields for demonstration
+    }
+    collection.insert_one(document)
+    return document["UUID"]
