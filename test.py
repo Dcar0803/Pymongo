a0901015aa6bbf16b62fec98f3917cbc9d83869d
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-02 21:12:56 -0500

    Test: Created a test function for saving a mongoDocument to the database.

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 def test_create_random_document():
     uuid = create_random_document()
     assert find_document_by_uuid(uuid) is not None
+
+
+def test_save_document():
+    document = {"UUID": str(uuid.uuid4()), "name": "TestDoc", "value": 200}
+    save_document(document)
+    assert find_document_by_uuid(document["UUID"]) == document
